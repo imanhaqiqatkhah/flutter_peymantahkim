98ce484bb8f63b1f44776ebf09a2f8e478c45d05
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-02-16 13:00:23 +0330

    add welcome

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,10 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_peymantahkim/theme/theme.dart';
+import 'package:flutter_peymantahkim/widgets/gradient_button.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+              image: DecorationImage(
+                image: AssetImage('./assets/images/welcome_bg.jpg'),
+                fit: BoxFit.cover,
+                opacity: 0.2,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Directionality(
+              textDirection: TextDirection.rtl,
+              child: Padding(
+                padding: EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Spacer(),
+                    Row(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(7),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Image.asset(
+                            './assets/icons/LOGO_PEYMAN-TAHKIM.png',
+                            width: 31,
+                            height: 31,
+                          ),
+                        ),
+                        SizedBox(width: 12),
+                        Text(
+                          'شرکت پیمان تحکیم خوزستان',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      'دارای\n مجموعه فنی و مهندسی',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.white,
+                        height: 1.2,
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      'جزء بالاترین رتبه شرکت های دانش بنیان ایران',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white.withOpacity(0.8),
+                        height: 1.5,
+                      ),
+                    ),
+                    SizedBox(height: 48),
+                    GradientButton(
+                      text: 'بزن بریم',
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) => SignUpScreen(),
+                        //   ),
+                        // );
+                      },
+                    ),
+                    SizedBox(height: 16),
+                    SizedBox(
+                      width: double.infinity,
+                    )
+                    // child: TextButton(onPressed: (){}, child: ),)
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
