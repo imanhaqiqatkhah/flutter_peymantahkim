5f1559f909653cd8b75c742c69a91da50472b830
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-03-11 11:59:51 +0330

    connect backend to mongodb

--- backend_api/index.js
+++ backend_api/index.js
@@ -1,16 +1,24 @@
 // import the express module
 const express = require("express");
-
 const helloRoute = require("./routes/hello");
+const mongoose = require("mongoose");
 
-// Port Server
+// port server
 const PORT = 3000;
 
 // create express application
 const app = express();
 
+// mongodb string
+const DB =
+  "mongodb+srv://peymantahkim:peyman7775@ptk.8jhmg.mongodb.net/?retryWrites=true&w=majority&appName=PTK";
+
 // middleware - to register routes or to mount routes
 app.use(helloRoute);
+
+mongoose.connect(DB).then(() => {
+  console.log("MongoDB Connected");
+});
 
 // start server listen to port
 app.listen(PORT, "0.0.0.0", function () {
