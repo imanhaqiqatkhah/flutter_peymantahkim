d49202c08703f078d9e61da7850632aadd5b4c43
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-03-12 14:17:10 +0330

    add user model in backend project

--- backend_api/routes/hello.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const helloRoute = express.Router();
-
-helloRoute.get("/hello", (req, res) => {
-  res.send("hello_world");
-});
-
-module.exports = helloRoute;
