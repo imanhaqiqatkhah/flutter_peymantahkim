f20d51707dc528c402506e62bb59b2df31ed6148
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-03-13 12:40:39 +0330

    added first part sign in section in backend

--- backend_api/routes/auth.js
+++ backend_api/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const authRouter = express.Router();
+const jwt = require("jsonwebtoken");
 
 authRouter.post("/api/signup", async (req, res) => {
   try {
@@ -25,6 +26,31 @@ authRouter.post("/api/signup", async (req, res) => {
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
+});
+
+// signin api endpoint
+
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const findUser = await User.findOne({ email });
+    if (!findUser) {
+      return res.status(400).json({ msg: "کاربر یافت نشد" });
+    } else {
+      const isMatch = await bcrypt.compare(password, findUser.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "رمز عبور اشتباه است" });
+      } else {
+        const token = jwt.sign({ id: findUser._id }, "passwordKey");
+
+        //remove sensitive information
+        const { password, ...userWithoutPassword } = findUser._doc;
+
+        // send the responses
+        res.json({ token, ...userWithoutPassword });
+      }
+    }
+  } catch (e) {}
 });
 
 module.exports = authRouter;
