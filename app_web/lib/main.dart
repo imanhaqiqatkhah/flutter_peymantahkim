bb72e12f5181fd5e01a63c00e64fe675767fb7b6
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-04-06 15:09:57 +0330

    added sidebar and title admin panel and I doing continue for next commits

--- app_web/lib/main.dart
+++ app_web/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_web/views/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -31,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MainScreen(),
     );
   }
 }
