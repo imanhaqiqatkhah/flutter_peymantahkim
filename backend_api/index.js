fffb4208a38a9e0dfe2e22b4cbe0bf80dd582c2f
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-04-06 10:47:34 +0330

    added models and route for res add products

--- backend_api/index.js
+++ backend_api/index.js
@@ -5,6 +5,7 @@ const authRouter = require("./routes/auth");
 const bannerRouter = require("./routes/banner");
 const categoryRouter = require("./routes/category");
 const subcategoryRouter = require("./routes/sub_category");
+const productRouter = require("./routes/product");
 
 // port server
 const PORT = 3000;
@@ -22,6 +23,7 @@ app.use(authRouter);
 app.use(bannerRouter);
 app.use(categoryRouter);
 app.use(subcategoryRouter);
+app.use(productRouter);
 
 mongoose.connect(DB).then(() => {
   console.log("MongoDB Connected");
