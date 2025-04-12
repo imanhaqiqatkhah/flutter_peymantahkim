4d1abbd37d3392709a0edffe80ee9400ef62f98a
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-04-12 14:38:57 +0330

    added image_picker on app_web

--- app_web/lib/views/side_bar_screens/category_screen.dart
+++ app_web/lib/views/side_bar_screens/category_screen.dart
@@ -1,15 +1,203 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class CategoryScreen extends StatelessWidget {
-  static const String id = "\categoryScreen";
+class CategoryScreen extends StatefulWidget {
+  static const String id = "categoryScreen";
   const CategoryScreen({super.key});
 
   @override
+  State<CategoryScreen> createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late String categoryName;
+  dynamic _bannerImage;
+  dynamic _image;
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+
+  pickBannerImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _bannerImage = result.files.first.bytes;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'فعالیت ها',
-        style: TextStyle(fontSize: 40),
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Container(
+                  alignment: Alignment.topRight,
+                  child: Center(
+                    child: Text(
+                      'دسته بندی ها',
+                      style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Divider(
+                  color: Colors.grey.shade400,
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.all(8),
+                          width: 130,
+                          height: 130,
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey.shade200,
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          child: Center(
+                            child: _image != null
+                                ? Image.memory(_image)
+                                : Text('عکس دسته بندی'),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              pickImage();
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue.shade900,
+                              foregroundColor: Colors.white,
+                            ),
+                            child: Text(
+                              'آپلود عکس',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: SizedBox(
+                        width: 180,
+                        child: TextFormField(
+                          onChanged: (value) {
+                            categoryName = value;
+                          },
+                          validator: (value) {
+                            if (value!.isNotEmpty) {
+                              return null;
+                            } else {
+                              return 'لطفا نام دسته بندی را وارد کنید';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            labelText: "نام دسته بندی را وارد کنید",
+                          ),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('لغو'),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue.shade900,
+                          foregroundColor: Colors.white),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print(categoryName);
+                        }
+                      },
+                      child: Text('ثبت'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Divider(
+              color: Colors.grey.shade400,
+            ),
+          ),
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(8),
+                width: 130,
+                height: 130,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                  child: _bannerImage != null
+                      ? Image.memory(_bannerImage)
+                      : Text(
+                          'بنر دسته بندی',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    pickBannerImage();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade900,
+                    foregroundColor: Colors.white,
+                  ),
+                  child: Text(
+                    'آپلود بنر',
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Divider(
+              color: Colors.grey.shade400,
+            ),
+          ),
+        ],
       ),
     );
   }
