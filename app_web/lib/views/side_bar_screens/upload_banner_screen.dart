3246ede2fb2f361c722211df2ee6779ec707c1a0
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-04-21 14:09:54 +0330

    fix json serialization in vscode

--- app_web/lib/views/side_bar_screens/upload_banner_screen.dart
+++ app_web/lib/views/side_bar_screens/upload_banner_screen.dart
@@ -1,16 +1,83 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class UploadBannerScreen extends StatelessWidget {
+class UploadBannerScreen extends StatefulWidget {
   static const String id = "\banner-screen";
   const UploadBannerScreen({super.key});
 
   @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  dynamic _image;
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'عکس بنر ها',
-        style: TextStyle(fontSize: 40),
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            alignment: Alignment.topRight,
+            child: Center(
+              child: Text(
+                "بنر ها",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2,
+        ),
+        Row(
+          children: [
+            Container(
+              margin: EdgeInsets.all(8),
+              width: 130,
+              height: 130,
+              decoration: BoxDecoration(
+                color: Colors.blueGrey.shade200,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Center(
+                child: _image != null
+                    ? Image.memory(_image)
+                    : Text('عکس دسته بندی'),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('ثبت'),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text('آپلود عکس'),
+          ),
+        ),
+      ],
     );
   }
 }
