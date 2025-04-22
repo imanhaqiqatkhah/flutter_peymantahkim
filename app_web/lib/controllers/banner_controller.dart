b06e60c02fe586c5a02661c53a8a141a6eb23935
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-04-22 15:28:39 +0330

    added banner widget

--- app_web/lib/controllers/banner_controller.dart
+++ app_web/lib/controllers/banner_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_web/global_variable.dart';
 import 'package:app_web/models/banner.dart';
 import 'package:app_web/services/manage_http_response.dart';
@@ -34,7 +36,35 @@ class BannerController {
             showSnackBar(context, 'آپلود شد');
           });
     } catch (e) {
-      print("Error uploading to cloudinary: $e");
+      print(e);
+    }
+  }
+
+  // fetch banners
+  Future<List<BannerModel>> loadBanners() async {
+    try {
+      // send an http get request to fetch banners
+      http.Response response = await http.get(
+        Uri.parse('$uri/api/banner'),
+        headers: <String, String>{
+          "Content-Type": 'application/json; charset=UTF-8'
+        },
+      );
+
+      print(response.body);
+      if (response.statusCode == 200) //ok
+      {
+        List<dynamic> data = jsonDecode(response.body);
+
+        List<BannerModel> banners =
+            data.map((banner) => BannerModel.fromJson(banner)).toList();
+        return banners;
+      } else {
+        ///throw an exception if the server responsed with an error status code
+        throw Exception('آپلود بنر با خطا همراه است');
+      }
+    } catch (e) {
+      throw Exception('باگ آپلود بنر، $e است');
     }
   }
 }
---
+++ app_web/lib/views/side_bar_screens/banner_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BannerWidget extends StatefulWidget {
+  const BannerWidget({super.key});
+
+  @override
+  State<BannerWidget> createState() => _BannerWidgetState();
+}
+
+class _BannerWidgetState extends State<BannerWidget> {
+  // a future that will hold list of banners once loaded from the API
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
