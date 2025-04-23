6c59120c1d130918e49ccdb1eb47174b783aa1bf
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-04-23 10:08:58 +0330

    change mainscreen to spashscreen

--- app_web/lib/views/side_bar_screens/banner_widget.dart
+++ app_web/lib/views/side_bar_screens/banner_widget.dart
@@ -35,18 +35,19 @@ class _BannerWidgetState extends State<BannerWidget> {
             );
           } else {
             final banners = snapshot.data!;
-            return SizedBox(
-              height: 400,
-              child: ListView.builder(
-                  itemCount: banners.length,
-                  itemBuilder: (context, index) {
-                    final banner = banners[index];
-                    return Image.network(
-                      width: 40,
-                      height: 40,
-                      banner.image,
-                    );
-                  }),
+            return GridView.builder(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 6, crossAxisSpacing: 8, mainAxisSpacing: 8),
+              itemCount: banners.length,
+              itemBuilder: (context, index) {
+                final banner = banners[index];
+                return Image.network(
+                  banner.image,
+                  height: 100,
+                  width: 100,
+                );
+              },
             );
           }
         });
