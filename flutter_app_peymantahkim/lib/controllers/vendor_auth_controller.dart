f9aebd3dd4108addbde1c683d36725964935a7f2
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-05-05 21:55:18 +0330

    added vendor needs first time

---
+++ flutter_app_peymantahkim/lib/controllers/vendor_auth_controller.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_app_peymantahkim/global_variables.dart';
+import 'package:flutter_app_peymantahkim/models/vendor.dart';
+import 'package:flutter_app_peymantahkim/services/manage_http_response.dart';
+
+import 'package:http/http.dart' as http;
+
+class VendorAuthController {
+  Future<void> signUpVendor(
+      {required String fullName,
+      required String email,
+      required String password,
+      required context}) async {
+    try {
+      Vendor vendor = Vendor(
+        id: '',
+        fullName: fullName,
+        email: email,
+        state: '',
+        city: '',
+        locality: '',
+        password: password,
+      );
+      http.Response response = await http.post(
+          Uri.parse('$uri/api/vendor/signup'),
+          body: vendor.toJson(),
+          headers: <String, String>{
+            // set headers for the request
+            "Content-Type": "application/json;  charset=UTF-8",
+          }); // convert the vendor user object to json for request body
+
+      // manage http response to handle http response base on the status code
+      manageHttpResponse(
+          response: response,
+          context: context,
+          onSuccess: () {
+            showSnackBar(context, 'اکانت کلاینت با موفقیت ایجاد شد');
+          });
+    } catch (e) {
+      print(e);
+    }
+  }
+}
