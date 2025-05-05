9966c7cc15db4c49754ec9591b568618f641c4af
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-05-05 22:44:30 +0330

    added splash and onboarding and welcome and login and register screens for vendor flutter project

--- flutter_app_peymantahkim/lib/controllers/vendor_auth_controller.dart
+++ flutter_app_peymantahkim/lib/controllers/vendor_auth_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter_app_peymantahkim/global_variables.dart';
 import 'package:flutter_app_peymantahkim/models/vendor.dart';
 import 'package:flutter_app_peymantahkim/services/manage_http_response.dart';
@@ -36,7 +38,37 @@ class VendorAuthController {
             showSnackBar(context, 'اکانت کلاینت با موفقیت ایجاد شد');
           });
     } catch (e) {
-      print(e);
+      showSnackBar(context, '$e');
+    }
+  }
+
+  /// function to consume the backend vendor signin api
+
+  Future<void> signInVendor(
+      {required String email,
+      required String password,
+      required context}) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse('$uri/api/vendor/signin'),
+        body: jsonEncode({
+          "email": email,
+          "password": password,
+        }),
+        headers: <String, String>{
+          // set headers for the request
+          "Content-Type": "application/json;  charset=UTF-8",
+        },
+      );
+
+      manageHttpResponse(
+          response: response,
+          context: context,
+          onSuccess: () {
+            showSnackBar(context, 'کلاینت با موفقیت وارد شد');
+          });
+    } catch (e) {
+      showSnackBar(context, '$e');
     }
   }
 }
--- flutter_peymantahkim/lib/views/widgets/splash_screen.dart
+++ flutter_peymantahkim/lib/views/widgets/splash_screen.dart
@@ -52,7 +52,7 @@ class _SplashScreenState extends State<SplashScreen>
           Navigator.pushReplacement(
               context,
               MaterialPageRoute(
-                builder: (context) => ChooseLanguageScreen(),
+                builder: (context) => OnboardingScreen(),
               ));
         }
       },
