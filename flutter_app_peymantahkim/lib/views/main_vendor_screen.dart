8bb24abe3e6b4860e7a3edfaf8383d69ad8e8238
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-05-07 10:45:54 +0330

    added bottom navigation vendor app

--- flutter_app_peymantahkim/lib/views/main_vendor_screen.dart
+++ flutter_app_peymantahkim/lib/views/main_vendor_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MainVendorScreen extends StatefulWidget {
@@ -8,12 +9,33 @@ class MainVendorScreen extends StatefulWidget {
 }
 
 class _MainVendorScreenState extends State<MainVendorScreen> {
+  int _pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Main Vendor Screen'),
-      ),
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              _pageIndex = value;
+            });
+          },
+          currentIndex: _pageIndex,
+          type: BottomNavigationBarType.fixed,
+          unselectedItemColor: Colors.grey,
+          selectedItemColor: const Color.fromARGB(255, 34, 26, 195),
+          showUnselectedLabels: false,
+          showSelectedLabels: true,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.money_dollar), label: 'دلار'),
+            BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.upload_circle), label: 'آپلود'),
+            BottomNavigationBarItem(icon: Icon(Icons.edit), label: 'ویرایش'),
+            BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.shopping_cart), label: 'سفارش ها'),
+            BottomNavigationBarItem(icon: Icon(Icons.logout), label: 'خروج'),
+          ]),
     );
   }
 }
