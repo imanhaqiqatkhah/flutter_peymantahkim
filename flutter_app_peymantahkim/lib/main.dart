68d27b82013848ed83688f7e4c34e717181e690d
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-05-07 09:32:24 +0330

    added WidgetRef into main.dart

--- flutter_app_peymantahkim/lib/main.dart
+++ flutter_app_peymantahkim/lib/main.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_peymantahkim/provider/vendor_provider.dart';
+import 'package:flutter_app_peymantahkim/views/main_vendor_screen.dart';
 import 'package:flutter_app_peymantahkim/views/widgets/splash_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    Future<void> checkTokenAndSetUser(WidgetRef ref) async {
+      // obtain an instance of shared preference
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+
+      // retrieve the authentication token and user data stored locally
+      String? token = preferences.getString('auth_token');
+      String? vendorJson = preferences.getString('vendor');
+
+      // if both the token and data are available, update the vendor state
+      if (token != null && vendorJson != null) {
+        ref.read(vendorProvider.notifier).setVendor(vendorJson);
+      } else {
+        ref.read(vendorProvider.notifier).signOut();
+      }
+    }
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -32,7 +52,18 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: FutureBuilder(
+        future: checkTokenAndSetUser(ref),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final vendor = ref.watch(vendorProvider);
+          return vendor != null ? MainVendorScreen() : SplashScreen();
+        },
+      ),
     );
   }
 }
