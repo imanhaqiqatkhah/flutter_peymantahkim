fa5d8accc550d9d380ab1051b34f3a6e392818a9
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-05-10 16:32:24 +0330

    added formKey into upload screen

--- flutter_vendor_peymantahkim/lib/views/screens/nav_screens/upload_screen.dart
+++ flutter_vendor_peymantahkim/lib/views/screens/nav_screens/upload_screen.dart
@@ -15,6 +15,7 @@ class UploadScreen extends StatefulWidget {
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   late Future<List<Category>> futureCategories;
   Future<List<Subcategory>>? futureSubcategories;
   Category? selectedCategory;
@@ -53,173 +54,237 @@ class _UploadScreenState extends State<UploadScreen> {
     // fetch subcategories base on the selected category
     futureSubcategories =
         SubcategoryController().getSubCategoriesByCategoryName(value.name);
+    // reset the selected subcategory
+    selectedSubcategory = null;
   }
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Directionality(
-        textDirection: TextDirection.rtl,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            GridView.builder(
-              shrinkWrap:
-                  true, //allow the gridview to shrink to fit the content
-              itemCount: images.length +
-                  1, // the number of items in the grid (+1 for the add button)
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                crossAxisSpacing: 4,
-                mainAxisSpacing: 4,
-                childAspectRatio: 1,
+      child: Form(
+        key: _formKey,
+        child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GridView.builder(
+                shrinkWrap:
+                    true, //allow the gridview to shrink to fit the content
+                itemCount: images.length +
+                    1, // the number of items in the grid (+1 for the add button)
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 4,
+                  mainAxisSpacing: 4,
+                  childAspectRatio: 1,
+                ),
+                itemBuilder: (context, index) {
+                  // if the index is 0, show the add icon button to new image
+                  return index == 0
+                      ? Center(
+                          child: IconButton(
+                              onPressed: () {
+                                chooseImage();
+                              },
+                              icon: Icon(Icons.add)),
+                        )
+                      : SizedBox(
+                          width: 50,
+                          height: 40,
+                          child:
+                              Image.file(images[index - 1], fit: BoxFit.cover),
+                        );
+                },
               ),
-              itemBuilder: (context, index) {
-                // if the index is 0, show the add icon button to new image
-                return index == 0
-                    ? Center(
-                        child: IconButton(
-                            onPressed: () {
-                              chooseImage();
-                            },
-                            icon: Icon(Icons.add)),
-                      )
-                    : SizedBox(
-                        width: 50,
-                        height: 40,
-                        child: Image.file(images[index - 1], fit: BoxFit.cover),
-                      );
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'نام محصول',
-                        border: OutlineInputBorder(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'نام محصول را وارد کنید';
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'نام محصول',
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    width: 200,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'قیمت محصول',
-                        border: OutlineInputBorder(),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: 200,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'قیمت محصول را وارد کنید';
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'قیمت محصول',
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    width: 200,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'تعداد محصول',
-                        border: OutlineInputBorder(),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: 200,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'تعداد محصول را وارد کنید';
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'تعداد محصول',
+                          border: OutlineInputBorder(),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  SizedBox(
-                    width: 200,
-                    child: FutureBuilder<List<Category>>(
-                      future: futureCategories,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (snapshot.hasError) {
-                          return Center(
-                            child: Text('خطا: ${snapshot.error}'),
-                          );
-                        } else if (!snapshot.hasData ||
-                            snapshot.data!.isEmpty) {
-                          return Center(
-                            child: Text('بدون مجموعه'),
-                          );
-                        } else {
-                          return DropdownButton<Category>(
-                              value: selectedCategory,
-                              hint: Text('انتخاب مجموعه'),
-                              items: snapshot.data!.map((Category category) {
-                                return DropdownMenuItem(
-                                  value: category,
-                                  child: Text(category.name),
-                                );
-                              }).toList(),
-                              onChanged: (value) {
-                                setState(() {
-                                  selectedCategory = value;
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: 200,
+                      child: FutureBuilder<List<Category>>(
+                        future: futureCategories,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text('خطا: ${snapshot.error}'),
+                            );
+                          } else if (!snapshot.hasData ||
+                              snapshot.data!.isEmpty) {
+                            return Center(
+                              child: Text('بدون مجموعه'),
+                            );
+                          } else {
+                            return DropdownButton<Category>(
+                                value: selectedCategory,
+                                hint: Text('انتخاب مجموعه'),
+                                items: snapshot.data!.map((Category category) {
+                                  return DropdownMenuItem(
+                                    value: category,
+                                    child: Text(category.name),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    selectedCategory = value;
+                                  });
+                                  getSubcategoryByCategory(selectedCategory);
                                 });
-                                getSubcategoryByCategory(selectedCategory);
-                              });
-                        }
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    width: 200,
-                    child: FutureBuilder<List<Subcategory>>(
-                      future: futureSubcategories,
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else if (snapshot.hasError) {
-                          return Center(
-                            child: Text('خطا: ${snapshot.error}'),
-                          );
-                        } else if (!snapshot.hasData ||
-                            snapshot.data!.isEmpty) {
-                          return Center(
-                            child: Text('بدون فعالیت'),
-                          );
-                        } else {
-                          return DropdownButton<Subcategory>(
-                              value: selectedSubcategory,
-                              hint: Text('انتخاب فعالیت'),
-                              items:
-                                  snapshot.data!.map((Subcategory subcategory) {
-                                return DropdownMenuItem(
-                                  value: subcategory,
-                                  child: Text(subcategory.subCategoryName),
-                                );
-                              }).toList(),
-                              onChanged: (value) {
-                                setState(() {
-                                  selectedSubcategory = value;
-                                });
-                              });
-                        }
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    width: 400,
-                    child: TextFormField(
-                      maxLines: 3,
-                      maxLength: 500,
-                      decoration: InputDecoration(
-                        alignLabelWithHint: true,
-                        labelText: 'توضیحات محصول',
-                        border: OutlineInputBorder(),
+                          }
+                        },
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      width: 200,
+                      child: FutureBuilder<List<Subcategory>>(
+                        future: futureSubcategories,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          } else if (snapshot.hasError) {
+                            return Center(
+                              child: Text('خطا: ${snapshot.error}'),
+                            );
+                          } else if (!snapshot.hasData ||
+                              snapshot.data!.isEmpty) {
+                            return Center(
+                              child: Text('بدون فعالیت'),
+                            );
+                          } else {
+                            return DropdownButton<Subcategory>(
+                                value: selectedSubcategory,
+                                hint: Text('انتخاب فعالیت'),
+                                items: snapshot.data!
+                                    .map((Subcategory subcategory) {
+                                  return DropdownMenuItem(
+                                    value: subcategory,
+                                    child: Text(subcategory.subCategoryName),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    selectedSubcategory = value;
+                                  });
+                                });
+                          }
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      width: 400,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'توضیحات محصول را وارد کنید';
+                          } else {
+                            return null;
+                          }
+                        },
+                        maxLines: 3,
+                        maxLength: 500,
+                        decoration: InputDecoration(
+                          alignLabelWithHint: true,
+                          labelText: 'توضیحات محصول',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: InkWell(
+                  onTap: () {
+                    if (_formKey.currentState!.validate()) {
+                      print('آپلود شد');
+                    } else {
+                      print('آپلود نشد');
+                    }
+                  },
+                  child: Container(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade900,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'آپلود محصول',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
