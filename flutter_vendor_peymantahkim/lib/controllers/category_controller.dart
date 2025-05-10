34164d04c6934d4386ff3b28d2d275898ad23aa1
imanhaqiqatkhah <imanhaqiqatkhah@gmail.com>
2025-05-10 14:11:12 +0330

    added upload screen dart and image picker file upload

---
+++ flutter_vendor_peymantahkim/lib/controllers/category_controller.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutter_vendor_peymantahkim/global_variables.dart';
+import 'package:flutter_vendor_peymantahkim/models/category.dart';
+
+import 'package:http/http.dart' as http;
+
+class CategoryController {
+  // load the upload category
+  Future<List<Category>> loadCategories() async {
+    try {
+      // send an http get request to load the categories
+      http.Response response = await http.get(
+        Uri.parse('$uri/api/categories'),
+        headers: <String, String>{
+          "Content-Type": 'application/json; charset=UTF-8'
+        },
+      );
+
+      print(response.body);
+
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+
+        List<Category> categories =
+            data.map((category) => Category.fromJson(category)).toList();
+        return categories;
+      } else {
+        throw Exception('خطا در بارگیری مجموعه ها');
+      }
+    } catch (e) {
+      print(e);
+      throw Exception('خطا در بارگیری مجموعه ها $e');
+    }
+  }
+}
--- flutter_vendor_peymantahkim/lib/views/screens/nav_screens/upload_screen.dart
+++ flutter_vendor_peymantahkim/lib/views/screens/nav_screens/upload_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_vendor_peymantahkim/controllers/category_controller.dart';
+import 'package:flutter_vendor_peymantahkim/models/category.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UploadScreen extends StatefulWidget {
@@ -11,6 +13,17 @@ class UploadScreen extends StatefulWidget {
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  late Future<List<Category>> futureCategories;
+  late String name;
+  Category? selectedCategory;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    futureCategories = CategoryController().loadCategories();
+  }
+
   // create an instance of the image picker to handle image selection
   final ImagePicker picker = ImagePicker();
 
@@ -35,35 +48,126 @@ class _UploadScreenState extends State<UploadScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GridView.builder(
-            shrinkWrap: true, //allow the gridview to shrink to fit the content
-            itemCount: images.length +
-                1, // the number of items in the grid (+1 for the add button)
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 4,
-              mainAxisSpacing: 4,
-              childAspectRatio: 1,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            GridView.builder(
+              shrinkWrap:
+                  true, //allow the gridview to shrink to fit the content
+              itemCount: images.length +
+                  1, // the number of items in the grid (+1 for the add button)
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                childAspectRatio: 1,
+              ),
+              itemBuilder: (context, index) {
+                // if the index is 0, show the add icon button to new image
+                return index == 0
+                    ? Center(
+                        child: IconButton(
+                            onPressed: () {
+                              chooseImage();
+                            },
+                            icon: Icon(Icons.add)),
+                      )
+                    : SizedBox(
+                        width: 50,
+                        height: 40,
+                        child: Image.file(images[index - 1], fit: BoxFit.cover),
+                      );
+              },
             ),
-            itemBuilder: (context, index) {
-              // if the index is 0, show the add icon button to new image
-              return index == 0
-                  ? Center(
-                      child: IconButton(
-                          onPressed: () {
-                            chooseImage();
-                          },
-                          icon: Icon(Icons.add)),
-                    )
-                  : SizedBox(
-                      width: 50,
-                      height: 40,
-                      child: Image.file(images[index - 1], fit: BoxFit.cover),
-                    );
-            })
-      ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'نام محصول',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'قیمت محصول',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: 200,
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'تعداد محصول',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  FutureBuilder(
+                    future: futureCategories,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      } else if (snapshot.hasError) {
+                        return Center(
+                          child: Text('خطا: ${snapshot.error}'),
+                        );
+                      } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                        return Center(
+                          child: Text('بدون مجموعه'),
+                        );
+                      } else {
+                        return DropdownButton<Category>(
+                            value: selectedCategory,
+                            hint: Text('انتخاب مجموعه'),
+                            items: snapshot.data!.map((Category category) {
+                              return DropdownMenuItem(
+                                value: category,
+                                child: Text(category.name),
+                              );
+                            }).toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                selectedCategory = value;
+                              });
+                              print(selectedCategory!.name);
+                            });
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    width: 400,
+                    child: TextFormField(
+                      maxLines: 3,
+                      maxLength: 500,
+                      decoration: InputDecoration(
+                        labelText: 'توضیحات محصول',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
